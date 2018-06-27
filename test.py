dce4fd39b6cf23cbcf9caeae54c71caa8c245377
MALLICK96 <40639248+MALLICK96@users.noreply.github.com>
2018-06-27 22:04:31 +0530

    add new project

---
+++ test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 27 13:14:19 2018
+
+@author: arind
+"""
+
+import pandas as pd
+from pandas import DataFrame
+df = pd.read_csv(r'C:\Users\arind\Desktop\sample_data.csv')
+print(df)
